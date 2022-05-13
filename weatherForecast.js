e65c9c0ded1faef69c1c0b52fa7d7f3fde76f45b
Matrice-ecole <kamal.guidadou>
2022-05-13 15:58:35 +0200

    Add final step

--- weatherForecast.js
+++ weatherForecast.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    
     // Créer input et bouton
-
+    
     let input = document.getElementById('search').value;
     let buttonClick = document.getElementById('search_button');
     let selectDays = document.getElementById('selectDays');
     let nDays = selectDays.options[selectDays.selectedIndex].value
     console.log(nDays)
-
-    // Créer l'event qui suit le click sur le bouton
-
+    
+    // Créer l'event qui suit le click sur le bouton //////////////////////////
+    
     buttonClick.addEventListener('click', function(event) {
-
+        
         event.preventDefault();
         
+        //////////// Récupérer éléments HTML pour affichage et déclarer les variables //////////
+        
+        let el = document.getElementById("result");
+        let newDiv = document.getElementById("day");
+
+        /////////// Initialisation des variables    /////////
+
+        newDiv.innerHTML = []
+        el.innerHTML = []
+
+        ////////////// Récupérer données de l'API OpenCageData par fetch /////////////
+
         const API_KEY = "c46400a6732a496faadf147afdbd17f8"
 
         let URL = `https://api.opencagedata.com/geocode/v1/json?q=${input}&key=${API_KEY}&language=fr&pretty=1`
@@ -29,11 +41,13 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             let result = data.results[0].geometry;
             console.log(result);
+        
+        /// déclarer latitude longitude /////////////////////////
 
             let lat = result.lat
             let lon = result.lng 
             
-        // Deuxième API; récupérer les data de geometry et afficher résultat en fonction de l'ID
+        // Deuxième API; récupérer les data d'OpenDataCage et afficher résultat en fonction de l'ID
         // i.e : if id = 800 innerhtml.src : icône du beau temps...
 
             const API_KEY_2 = "c512e8d95b8c68fbc1f680cf5953680f"
@@ -54,15 +68,17 @@ document.addEventListener("DOMContentLoaded", function() {
                     console.log(data2)
 
                     let selectDays = document.getElementById('selectDays');
-                    let nDays = selectDays.options[selectDays.selectedIndex].value
+                    let nDays = selectDays.value
                     console.log(nDays)
+
+                    //////// Initialisation de la boucle pour affichage plusieurs jours ////
                     
                     for(i=0; i<nDays; i++) {
 
                         let newResult = data2.daily[i].weather[0].id;
                         console.log(newResult)
                         
-                        //////////// Changer background et font si jour ou nuit //////////////
+                        //////////// Changer background et police si jour ou nuit //////////////
                         
                         let uv = data2.current.uvi;
                         console.log(uv);
@@ -70,13 +86,12 @@ document.addEventListener("DOMContentLoaded", function() {
                         if(uv == 0) {
                             
                             document.body.style.backgroundImage ="url('./background/background_night.jpg')"
-                            document.getElementById("day").style.color = "#ffffff";
+                            document.getElementById("day").style.color = "#ff0000";
                             
                         }
                         
                         //////////////// Changer icone meteo en fct de l'id /////////////////////
 
-                        let el = document.getElementById("result");
                         let element2 = document.createElement("div");
 
                         if ( newResult == 800) {
@@ -88,9 +103,6 @@ document.addEventListener("DOMContentLoaded", function() {
                             else if (newResult > 800 ) {
                                 el.appendChild(element2).innerHTML = "<img src=\"./icons/cloudy.svg\" width=\"200px\" height=\"75px\">";
                         }
-                        //     else if (newResult == 803 && newResult == 804) {
-                        //         el.appendChild(element2).innerHTML = "<img src=\"./icons/clouds.svg\" width=\"200px\" height=\"75px\">";
-                        // }
                             else if ( newResult >= 500 && newResult <= 531) {
                                 el.appendChild(element2).innerHTML = "<img src=\"./icons/rain.svg\" width=\"200px\" height=\"75px\">";
                         }
@@ -105,7 +117,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
                         let day = week[curr_day + i];
                         let element = document.createElement("h3");
-                        let newDiv = document.getElementById("day");
                         newDiv.appendChild(element).innerHTML = `${day}`;
                         console.log(day);
                     }
